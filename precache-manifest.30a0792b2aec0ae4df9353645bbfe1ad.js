620b221f2658666dc835a15a3e736c1d5bab73b2
Singhasha1 <112199624+Singhasha1@users.noreply.github.com>
2023-05-03 23:27:53 +0530

    Updates

---
+++ precache-manifest.30a0792b2aec0ae4df9353645bbfe1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d60a10d8436db1ded938",
+    "url": "/Social-App/static/css/main.063970dc.chunk.css"
+  },
+  {
+    "revision": "d60a10d8436db1ded938",
+    "url": "/Social-App/static/js/main.d60a10d8.chunk.js"
+  },
+  {
+    "revision": "b151e9bcad69e5975fb1",
+    "url": "/Social-App/static/js/1.b151e9bc.chunk.js"
+  },
+  {
+    "revision": "117994b0cab496d2a50f",
+    "url": "/Social-App/static/js/2.117994b0.chunk.js"
+  },
+  {
+    "revision": "ac476c9acebd22287942",
+    "url": "/Social-App/static/js/runtime~main.ac476c9a.js"
+  },
+  {
+    "revision": "a38375856dbdcea46a21424f48e683d5",
+    "url": "/Social-App/index.html"
+  }
+];
